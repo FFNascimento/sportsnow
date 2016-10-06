9dae3ec885a08e0a0d299154ce7caefdc13b1b1e
Fabricio Nascimento <Fabricio Nascimento>
2016-10-05 21:22:49 -0300

    Detalhes do produto

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 var express = require('express');
 
 var app = express();
+
+
+app.get('/',function (req,res) {
+
+});
+
+app.listen(3000, function () {
+  console.log('Sports Now on port 3000!');
+});
