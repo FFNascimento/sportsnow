c4d3c7d8c0d8d38dd6f4738a664bd074143993a6
Gabriel Garcia <gabrielgfa1@gmail.com>
2016-11-15 18:04:56 -0200

    Negódi funcionando a rota.

--- client/components/controllers/ProductListController.js
+++ client/components/controllers/ProductListController.js
@@ -1,19 +1,23 @@
 'use strict';
 
 angular.module('app.productList', ['ui.router', 'ngRoute'])
-.controller('ProductListController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout, $stateParams) {
+.controller('ProductListController', ['$scope', '$http', '$timeout', '$stateParams', function($scope, $http, $timeout, $stateParams) {
     
 	alert("eita");
+
 
 	$scope.products = {};
 
     $scope.getAPIData = function() {
         $http({
             method: 'GET',
-            url: 'get/products/filter/' + $stateParams.type
+            url: '/get/products/filter/MASCULINO'
         }).then(function success(res) {
             $scope.products = res;
-        }, function error(err) {});
+            alert(JSON.stringify($scope.products));
+        }, function error(err) {
+            alert(JSON.stringify(err));
+        });
     };
 
     $scope.getAPIData();
