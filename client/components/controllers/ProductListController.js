4f9ba875524ca9bdf7a73b072c23a8145907dc99
Gabriel Garcia <gabrielgfa1@gmail.com>
2016-11-15 17:40:32 -0200

    Angular, te odeio.

---
+++ client/components/controllers/ProductListController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('app.productList', ['ui.router', 'ngRoute'])
+.controller('ProductListController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout, $stateParams) {
+    
+	alert("eita");
+
+	$scope.products = {};
+
+    $scope.getAPIData = function() {
+        $http({
+            method: 'GET',
+            url: 'get/products/filter/' + $stateParams.type
+        }).then(function success(res) {
+            $scope.products = res;
+        }, function error(err) {});
+    };
+
+    $scope.getAPIData();
+}]);
