8022f820436770813565e385bf7790c78634e101
leynerbueno <leyner_henrique@hotmail.com.br>
2016-12-08 01:30:08 -0200

    Relatorio Usuario - listagem de usuarios

--- client/components/controllers/admin/RelatorioUsuariosController.js
+++ client/components/controllers/admin/RelatorioUsuariosController.js
@@ -17,24 +17,12 @@ angular.module('dashboard.relatorioUsuarios', ['app.user.service'])
 .controller('RelatorioUsuariosController', ['$scope','userService', '$rootScope',
     function($scope, userService, $rootScope) {
       $scope.users = null;
-      $scope.usersBase = {
-        permission: "",
-        name: "",
-        cpf: "",
-        rg: "",
-        endereco: "",
-        email: "",
-        type: "",
-      };
-      $scope.currentUser = null;
       $scope.title = userService.messageList().mainTitle;
 
       $scope.getAllUsers = function() {
         userService.getAllUsers().then(function success(res) {
           console.log(res);
           $scope.users = res.data;
-          console.log("objeto users preenchido");
-          console.log($scope.users);
         }, function error(err) {
           console.log(err);
         });
